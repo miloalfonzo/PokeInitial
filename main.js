9141ee91c27ae15e6b7c87cfa78b094e182522f6
Milo <milagrosalfonzo@outlook.com.ar>
2020-03-21 18:07:43 -0300

    v1

---
+++ main.js
@@ -0,0 +1,51 @@
+function play() {
+    var audio = document.getElementById("audio");
+    audio.volume = 0.1;
+    audio.play();
+}
+
+var eleccion = prompt('Profesor Oak: Es hora de elegir a tu compañero. Elige entre el 1, 2 y 3');
+
+var imagenes = ["https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png", "https://vignette.wikia.nocookie.net/es.pokemon/images/4/43/Bulbasaur.png/revision/latest?cb=20170120032346", "https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c32a.png"];
+
+
+const buttonSwitch = document.querySelector("#switch");
+
+buttonSwitch.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    buttonSwitch.classList.toggle("active");
+    switch(eleccion){
+        case "1":
+             document.getElementById("img1").src = imagenes[0];
+        break;
+        case "2":
+            document.getElementById("img1").src = imagenes[1];
+        break;
+        case "3":
+            document.getElementById("img1").src = imagenes[2];
+        break;
+        default:
+            alert("Solo tienes tres opciones, vuelve a elegir.");
+            
+    }  
+    
+    if (document.body.className == "body"){
+    document.getElementById("img1").src = "https://www.stickpng.com/assets/images/580b57fcd9996e24bc43c31f.png" ;
+} 
+})
+
+var whichOne = document.querySelector("#img1");
+
+whichOne.addEventListener("click", () => {
+    switch(eleccion){
+        case "1":
+            alert("¡Es un Charmander!");
+        break;
+        case "2":
+            alert("¡Es un Bulbasaur!");
+        break;
+        case "3":
+            alert("¡Es un Squirtle!")
+        break;
+    }
+})
